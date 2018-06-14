a6230e1d51904dcc1d37b1561ceff10a674c7b77
Cheri Allen <cherimarie@gmail.com>
2018-06-14 14:25:58 -0700

    Improve appearance

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,8 @@ class Event < ApplicationRecord
   validates :name, :category, presence: true
   validates :name, uniqueness: { scope: :category, message: "already exists", case_sensitive: false }
   validates :category, inclusion: {in: ["activity", "mood"]}
+
+  def label
+    return "#{category}: #{name}"
+  end
 end
